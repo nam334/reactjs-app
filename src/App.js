29024ee98199c8b6b563d49504a60c3575aec69d
nam334 <namratadas334@gmail.com>
2022-11-24 12:54:26 +0530

    added namaste react assignment 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Heading from './components/Heading';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <h3><b>Using React.createElement</b></h3>
+      { React.createElement( "div", {id:"title"}, [
+        React.createElement( "h4", {style:{ color: "black"}}, "This is the first heading"),
+        React.createElement( "h5", {style:{ color: "black"}}, "This is the second heading"),
+        React.createElement( "h6", {style:{ color: "black"}}, "This is the third heading")
+        
+      ])}
+      <br/>
+    <h3><b>Using JSX</b></h3>
+    {
+    <span>
+    <h4>This is the first heading</h4>
+    <h5>This is the second heading</h5>
+    <h6>This is the third heading</h6>
+    </span>
+    }
+    <br/>
+    <h3><b>Using Functional Component</b></h3>
+    <Heading/>
     </div>
   );
 }
