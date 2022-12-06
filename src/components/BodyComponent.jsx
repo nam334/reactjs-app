53496bc938790b791d26c3a4680cf5710b642cd8
nam334 <namratadas334@gmail.com>
2022-12-06 11:07:38 +0530

    branch is updated till assignment 5

---
+++ src/components/BodyComponent.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import CardComponent from './CardComponent'
+import data from '../data.json'
+import SearchBar from './SearchBar'
+const BodyComponent = () => {
+   const [searchData, setsearchData] = useState(data)
+      
+  return ( 
+    <>
+    <SearchBar setsearchData={setsearchData}/>
+    <div className='card-container'>
+    {
+      searchData.map((items,index)=>{return <CardComponent items={items}/>})
+    }
+    </div>
+    </>
+   
+  )
+}
+
+export default BodyComponent
